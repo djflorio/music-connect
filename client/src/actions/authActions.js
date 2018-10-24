8ea5efadab35cd07db1e849be13ac892d812a35d
Dan Florio <dflo@danflorio.com>
2018-10-24 17:39:39 -0400

    add login redux logic

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,12 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
-import { UPDATE_REGISTER_ERRORS } from "./types";
+import {
+  UPDATE_REGISTER_ERRORS,
+  UPDATE_LOGIN_ERRORS,
+  SET_CURRENT_USER
+} from "./types";
 
 // Register user
 export const registerUser = (userData, history) => {
@@ -13,3 +19,33 @@ export const registerUser = (userData, history) => {
       });
   };
 };
+
+// Login - Get user token
+export const loginUser = userData => {
+  return dispatch => {
+    axios
+      .post("/api/users/login", userData)
+      .then(res => {
+        const { token } = res.data;
+        localStorage.setItem("jwtToken", token);
+        // Set token to auth header
+        setAuthToken(token);
+        // Decode token to get user data
+        const decoded = jwt_decode(token);
+        // Set current user
+        dispatch(setCurrentUser(decoded));
+      })
+      .catch(err =>
+        dispatch({
+          type: UPDATE_LOGIN_ERRORS,
+          payload: err.response.data
+        })
+      );
+  };
+};
+
+// Set logged in user
+export const setCurrentUser = decoded => ({
+  type: SET_CURRENT_USER,
+  payload: decoded
+});
