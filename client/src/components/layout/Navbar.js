72d690d5173fcebb86e0b2a0043ea6c553e320f9
Dan Florio <dflo@danflorio.com>
2018-10-28 22:17:11 -0400

    add profile fetching

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -102,5 +104,5 @@ function mapStateToProps(state) {
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(withRouter(Navbar));
