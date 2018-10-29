ab1cec6b3441631bdfbfba32605c13ea2e578847
Dan Florio <dflo@danflorio.com>
2018-10-29 14:43:47 -0400

    add prettier to build

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,13 +6,11 @@ import { getCurrentProfile } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 
 class Dashboard extends Component {
-
   componentDidMount() {
     this.props.getCurrentProfile();
   }
 
   render() {
-
     const { user } = this.props.auth;
     const { profile, loading } = this.props.profile;
 
@@ -22,7 +20,7 @@ class Dashboard extends Component {
     } else {
       // Check if logged in user has profile data
       if (Object.keys(profile).length > 0) {
-        dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>
+        dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>;
       } else {
         // User is logged in but has no profile
         dashboardContent = (
@@ -56,7 +54,7 @@ Dashboard.propTypes = {
   profile: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
   getCurrentProfile: PropTypes.func.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   profile: state.profile,
